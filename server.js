e7a0a4bf4feffc11300b6c94513e231c71bc5bc9
Vimal Madhavan <vimal_m@Vimal-Madhavan.local>
2019-12-30 19:33:46 +0400

    Cors issue - proxy through node

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 5000;
 const app = express();
+const request = require("request");
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + "/dist"));
 
 // send the user to index html page inspite of the url
-app.get('*', cors(), (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get("/", cors(), (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/dist", "index.html"));
+});
 
+app.get("/samtliga", cors(), (req, res) => {
+  const URL = "https://content.viaplay.se/pc-se/serier/samtliga";
+  request(URL, (error, response, body) => {
+    res.send(body);
+  });
 });
 
 app.listen(port);
--- src/hooks/useSeries.js
+++ src/hooks/useSeries.js
@@ -1,29 +1,31 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const URL = 'https://content.viaplay.se/pc-se/serier/samtliga';
+const URL = "/samtliga";
 /**
  * Hook to fetch movie series
  */
 const useSeries = () => {
-    const [response, setResponse] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-    async function fetchSeries(){
-        setError(null);
-        setLoading(true);
-        try{
-            const response = await fetch(URL);
-            const data = await response.json();
-            window.data = data;
-            setResponse(data._embedded['viaplay:blocks'][0]._embedded['viaplay:products']);
-            setLoading(false);
-        } catch(e){
-            setError(`${e.status}: ${e.statusText}`);
-            setLoading(false);
-        }
+  async function fetchSeries() {
+    setError(null);
+    setLoading(true);
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      window.data = data;
+      setResponse(
+        data._embedded["viaplay:blocks"][0]._embedded["viaplay:products"]
+      );
+      setLoading(false);
+    } catch (e) {
+      setError(`${e.status}: ${e.statusText}`);
+      setLoading(false);
     }
-    return [response, loading, error, fetchSeries];
-}
+  }
+  return [response, loading, error, fetchSeries];
+};
 
 export default useSeries;
